a28d5d2901e9d532c74a0b6327c47d991277c22a
JuanCabello163 <cabellojuan717@gmail.com>
2023-08-17 12:42:55 -0500

    le agregamos la resta

--- grupito/src/main/java/com/mycompany/grupito/Grupito.java
+++ grupito/src/main/java/com/mycompany/grupito/Grupito.java
@@ -30,5 +30,20 @@ public class Grupito {
                 
             
         System.out.println();     
+        
+        //RESTAAAAAA
+        System.out.println("La resta");
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        
+        int numero3 = scanner.nextInt();
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero4 = scanner.nextInt();
+        
+        int resta = numero3 - numero4;
+        System.out.println("La resta de " + numero3 + " - " + numero4 + " es: " + resta);
+        
+        System.out.println();
+                
     }
 }
